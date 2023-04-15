a473caff03d90c6366baf47e226aaef69628807a
saibavi23 <bhavyansai23@gmail.com>
2023-04-15 11:53:07 -0600

    Initial Commit

---
+++ src/main/java/com/example/urlshortener/UrlShortenerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerExampleApplication.class, args);
+	}
+}
